fd3074ec20a024bf96813315aa028cc15cda37e9
CodeWracker <dev.rodrigofs@gmail.com>
2022-12-13 15:45:17 -0300

    coloquei os arquivos basicos

---
+++ main/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <map>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "serial.hpp"
+#include <inttypes.h>
+
+extern "C" void app_main();
+int v=0;
+
+void app_main()
+{
+    serial.begin(9600);
+    printf("Inicio");
+    
+}
