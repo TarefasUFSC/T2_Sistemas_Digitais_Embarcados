0f787af813941c5429a8210ec84e60107dccfdbc
CodeWracker <dev.rodrigofs@gmail.com>
2022-12-13 16:29:26 -0300

    fiz o menu

--- main/main.cpp
+++ main/main.cpp
@@ -10,14 +10,50 @@
 #include <inttypes.h>
 
 extern "C" void app_main();
-int v=0;
+int v = 0;
+
+using namespace std;
+
+void printMenu(){
+    
+    printf(" \n");
+    printf("[1] - Ajusta set point\n");
+    printf("[2] - Mede altura\n");
+}
+
+void ajustaSetPoint()
+{
+    printf("Ajusta SetPoint\n");
+}
+void medeAltura()
+{
+    printf("Mede Altura\n");
+}
+
+
 
 void app_main()
 {
     serial.begin(9600);
-    while(1){
-        
-    printf("Inicio\n");
+
+    typedef void (*funcP)();
+    map<char, funcP> myMap;
+    myMap['1'] = ajustaSetPoint;
+    myMap['2'] = medeAltura;
+
+    while (1)
+    {
+        printMenu();
+
+        char choice = serial.readChar();
+        if (myMap.find(choice) != myMap.end())
+        {
+            printf("choice: %c\n", choice);
+            myMap[choice]();
+        }
+        else
+        {
+            printf("Opcao invalida\n\n\n");
+        }
     }
-    
 }
