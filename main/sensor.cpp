b577c14ad45709572c796a18ae07fda1cb8aeeed
CodeWracker <dev.rodrigofs@gmail.com>
2022-12-13 21:28:33 -0300

    algumas melhorias no codigo

--- main/sensor.cpp
+++ main/sensor.cpp
@@ -8,22 +8,21 @@
 #include "delay.h"
 #include <inttypes.h>
 
-gpio_num_t trigPin;
-gpio_num_t echoPin;
+
 
 Sensor::Sensor(gpio_num_t pino_trigger, gpio_num_t pino_echo)
 {
     this->baseHeightSet = false;
     this->baseHeight = 0;
-    trigPin = pino_trigger;
-    echoPin = pino_echo;
+    this->trigPin = pino_trigger;
+    this->echoPin = pino_echo;
 }
 void Sensor::init()
 {
 
-    digital.pinMode(trigPin, OUTPUT);
-    digital.pinMode(echoPin, INPUT);
-    digital.digitalWrite(trigPin, LOW);
+    digital.pinMode(this->trigPin, OUTPUT);
+    digital.pinMode(this->echoPin, INPUT);
+    digital.digitalWrite(this->trigPin, LOW);
 }
 
 void Sensor::setupBaseHeight(int newHeight)
@@ -31,37 +30,55 @@ void Sensor::setupBaseHeight(int newHeight)
     this->baseHeight = newHeight;
     this->baseHeightSet = true;
 }
-
+int Sensor::getTimeDelta()
+{
+    int t1 = esp_timer_get_time(); // pega o tempo atual em microsegundos
+    int t2;
+    int duracao = 0;
+    this->waitPulseChangeTo(LOW);
+    t2 = esp_timer_get_time();
+    duracao = (t2 - t1);
+    return duracao;
+}
+int Sensor::calculateDistance(int timeDelta)
+{
+    int distancia;
+    distancia = ((timeDelta) / 2) / 29.1;
+    return distancia;
+}
+void Sensor::waitPulseChangeTo(int targetState)
+{
+    while (digital.digitalRead(this->echoPin) != targetState)
+    {
+    }
+}
+void Sensor::triggerMeasurement()
+{
+    digital.digitalWrite(this->trigPin, HIGH);
+    delay_us(10);
+    digital.digitalWrite(this->trigPin, LOW);
+}
+int Sensor::convertToBaseHeight(int distance)
+{
+    if (this->baseHeightSet)
+    {
+        distance = distance - this->baseHeight;
+    }
+    return distance;
+}
 int Sensor::getDistance()
 {
     // envia um pulso de 10us no pino trig
-    digital.digitalWrite(trigPin, HIGH);
-    delay_us(10);
-    digital.digitalWrite(trigPin, LOW);
-    while (digital.digitalRead(echoPin) == LOW)
-    {
-    }
+    this->triggerMeasurement();
+    this->waitPulseChangeTo(HIGH);
 
     // mede o tempo que o pino echo ficou em HIGH
-    int t1 = esp_timer_get_time(); // pega o tempo atual em microsegundos
 
-    int t2;
-    int duracao = 0;
-    while (digital.digitalRead(echoPin) == HIGH)
-    {
-    }
-    t2 = esp_timer_get_time();
-    duracao = (t2 - t1);
-    // printf("duracao: %d \n", duracao);
+    int duracao = this->getTimeDelta();
 
     // calcula a distancia em cm
-    int distancia;
-    distancia = ((duracao) / 2) / 29.1;
-    printf("distancia: %d \n", distancia);
-    if (this->baseHeightSet)
-    {
-        distancia = this->baseHeight - distancia;
-    }
+    int distancia = this->calculateDistance(duracao);
+    distancia = this->convertToBaseHeight(distancia);
     return distancia;
 }
 
--- main/sensor.hpp
+++ main/sensor.hpp
@@ -12,20 +12,29 @@
 #include "digital.h"
 #include "delay.h"
 
+class Sensor
+{
+private:
+    gpio_num_t trigPin;
+    gpio_num_t echoPin;
+    int baseHeight;
+    bool baseHeightSet;
 
+public:
+    Sensor();
+    Sensor(gpio_num_t pino_trigger, gpio_num_t pino_echo);
+    ~Sensor();
 
-class Sensor{
-    private:
-        int baseHeight;
-        bool baseHeightSet;
-    public:
-        Sensor();
-        Sensor(gpio_num_t pino_trigger, gpio_num_t pino_echo);
-        ~Sensor();
+    void init();
+    void setupBaseHeight(int newHeight);
+    int getDistance();
 
-        void init();
-        void setupBaseHeight(int newHeight);
-        int getDistance();
+private:
+    int getTimeDelta();
+    int calculateDistance(int timeDelta);
+    void waitPulseChangeTo(int targetState);
+    void triggerMeasurement();
+    int convertToBaseHeight(int distance);
 };
 
 #endif
